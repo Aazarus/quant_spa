28f18c94c8651e1c5b19f1eacae6f9f9b2432d41
Aaron <aaron@sevnasoftware.com>
2022-05-09 22:59:18 +0100

    Added OneWayBinding functionality

--- spa/src/app/chapter02/one-way-binding/one-way-binding.component.ts
+++ spa/src/app/chapter02/one-way-binding/one-way-binding.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Candle from 'src/app/models/candle';
 
 @Component({
   selector: 'app-one-way-binding',
@@ -9,8 +10,25 @@ export class OneWayBindingComponent implements OnInit {
 
   title: string = 'One Way Binding';
 
+  public candle: Candle = {
+    ticker: 'IBM',
+    date: new Date('07/14/2015'),
+    close: 168.61,
+    high: 169.54,
+    low: 168.24,
+    open: 169.43
+  };
+
   constructor() { }
 
-  ngOnInit() {}
+  public ngOnInit(): void {}
+
+  public getColor(price: number): string {
+    return price < 169 ? 'red' : 'green';
+  }
+
+  public avgPrice(candle: Candle): number {
+    return (candle.close + candle.high + candle.low + candle.open)/4;
+  }
 
 }
