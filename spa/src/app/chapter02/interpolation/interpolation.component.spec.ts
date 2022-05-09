e90e6a7aca0a6d7d8b7e232e9e4fa1009ecaf0da
Aaron <aaron@sevnasoftware.com>
2022-05-09 23:11:35 +0100

    Added tests to Interpolation and OneWayBinding

--- spa/src/app/chapter02/interpolation/interpolation.component.spec.ts
+++ spa/src/app/chapter02/interpolation/interpolation.component.spec.ts
@@ -1,11 +1,30 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import Candle from 'src/app/models/candle';
 
 import { InterpolationComponent } from './interpolation.component';
 
 describe('InterpolationComponent', () => {
   let component: InterpolationComponent;
   let fixture: ComponentFixture<InterpolationComponent>;
+  let candles: Candle[] = [
+    {
+      ticker: 'IBM',
+      date: new Date('07/14/2015'),
+      close: 168.61,
+      high: 169.54,
+      low: 168.24,
+      open: 169.43
+    },
+    {
+      ticker: 'AAP',
+      date: new Date('07/14/2015'),
+      close: 167.61,
+      high: 169.74,
+      low: 167.24,
+      open: 169.03
+    },
+  ];
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -20,5 +39,18 @@ describe('InterpolationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.candles).toEqual(candles);
+  });
+
+  describe('avgPrice', () => {
+    it('should return the average price of a candle', () => {
+      // Arrange
+      const expected = 168.405;
+      // Act
+      const actual = component.avgPrice(candles[1]);
+
+      // Assert
+      expect(actual).toBe(expected);
+    });
   });
 });
--- spa/src/app/chapter02/one-way-binding/one-way-binding.component.spec.ts
+++ spa/src/app/chapter02/one-way-binding/one-way-binding.component.spec.ts
@@ -1,11 +1,20 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import Candle from 'src/app/models/candle';
 import { OneWayBindingComponent } from './one-way-binding.component';
 
 describe('OneWayBindingComponent', () => {
   let component: OneWayBindingComponent;
   let fixture: ComponentFixture<OneWayBindingComponent>;
+  let candle: Candle = 
+  {
+    ticker: 'IBM',
+    date: new Date('07/14/2015'),
+    close: 167.61,
+    high: 169.74,
+    low: 167.24,
+    open: 169.03
+  };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -20,5 +29,46 @@ describe('OneWayBindingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('getColor', () => {
+
+    it('should return green if provided number is 169 or greater', () => {
+      
+      // Arrange
+      const price = 169.01;
+      const expected = 'green';
+
+      // Act
+      const actual = component.getColor(price);
+
+      // Assert
+      expect(actual).toBe(expected);
+    });
+
+    it('should return red if provided number is less than 169', () => {
+      
+      // Arrange
+      const price = 168.91;
+      const expected = 'red';
+
+      // Act
+      const actual = component.getColor(price);
+
+      // Assert
+      expect(actual).toBe(expected);
+    });
+  });
+
+  describe('avgPrice', () => {
+    it('should return the average price of a candle', () => {
+      // Arrange
+      const expected = 168.405;
+      // Act
+      const actual = component.avgPrice(candle);
+
+      // Assert
+      expect(actual).toBe(expected);
+    });
   });
 });
