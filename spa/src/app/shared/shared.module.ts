3d59e87a188a3f37b2272c0664eb4e38aeddd28a
Aaron <aaron@sevnasoftware.com>
2022-05-10 22:56:27 +0100

    Added new common header component

---
+++ spa/src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+const components = [
+    HeaderComponent
+];
+
+@NgModule({
+  declarations: components,
+  exports: components,
+})
+export class SharedModule {}
