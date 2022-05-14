169c783a73d183f1331952ea3aa1e8d53c3a209c
Aaron <aaron@sevnasoftware.com>
2022-05-14 16:28:37 +0100

    Added content to demo to way binding

--- spa/src/app/chapter02/two-way-binding/two-way-binding.component.ts
+++ spa/src/app/chapter02/two-way-binding/two-way-binding.component.ts
@@ -1,4 +1,6 @@
+import { CandleService } from 'src/app/services/candle.service';
 import { Component, OnInit } from '@angular/core';
+import Candle from 'src/app/models/candle';
 
 @Component({
   selector: 'app-two-way-binding',
@@ -9,7 +11,16 @@ export class TwoWayBindingComponent implements OnInit {
 
   public title: string = "Two Way Binding";
 
-  constructor() { }
+  public candle: Candle = {
+    ticker: 'IBM',
+    date: new Date('07/14/2015'),
+    close: 168.61,
+    high: 169.54,
+    low: 168.24,
+    open: 169.43
+  };
+
+  constructor(public candleService: CandleService) { }
 
   ngOnInit() {}
 
