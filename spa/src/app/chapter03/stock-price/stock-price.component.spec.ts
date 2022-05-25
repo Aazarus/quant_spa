78623e2b04d98dcf468b8cdb2914c68de7c84b61
Aaron <aaron@sevnasoftware.com>
2022-05-25 22:03:21 +0100

    Added content and tests for stock price component

--- spa/src/app/chapter03/stock-price/stock-price.component.spec.ts
+++ spa/src/app/chapter03/stock-price/stock-price.component.spec.ts
@@ -1,29 +1,118 @@
+import { symbolTestData, symbolAndPriceTestData } from './../../tests/test-data';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { RepositoryService } from 'src/app/services/repository.service';
 
 import { StockPriceComponent } from './stock-price.component';
+import { Symbol } from 'src/app/models/symbol';
 
 describe('StockPriceComponent', () => {
   let component: StockPriceComponent;
   let fixture: ComponentFixture<StockPriceComponent>;
+  let repoServiceSpy: jasmine.SpyObj<RepositoryService>;
 
   beforeEach(waitForAsync(() => {
+    const repoServiceSpyObj = jasmine.createSpyObj('RepositoryService', 
+        ['getStockAndPrice', 'getStockAndPriceWithTicker'], 
+        {
+          'stock': symbolTestData[0],
+          'stockWithTicker': symbolTestData[1]
+        }
+      );
+
     TestBed.configureTestingModule({
       declarations: [StockPriceComponent],
       imports: [
         IonicModule.forRoot(),
         HttpClientTestingModule
+      ],
+      providers: [
+        {provide: RepositoryService, useValue: repoServiceSpyObj}
       ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(StockPriceComponent);
     component = fixture.componentInstance;
+
+    repoServiceSpy = TestBed.inject(RepositoryService) as jasmine.SpyObj<RepositoryService>;
+
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
     expect(component.title).toBe("Stock Price");
+  });
+
+  describe('ngOnInit', () => {
+    it('should call getStockAndPrice and getStockAndPriceWithTicker', () => {
+      // Arrange
+      spyOn(component, 'getStockAndPrice');
+      spyOn(component, 'getStockAndPriceWithTicker');
+
+      // Act
+      component.ngOnInit();
+
+      // Assert
+      expect(component.getStockAndPrice).toHaveBeenCalled();
+      expect(component.getStockAndPriceWithTicker).toHaveBeenCalled();
+    });
+  });
+
+  describe('stock getter', () => {
+    it('should return the stock held by the RepositoryService', () => {
+      // Arrange
+      const expected: Symbol = symbolTestData[0];
+
+      // Act
+      var actual = component.stock;
+
+      // Assert
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('stockWithTicker getter', () => {
+    it('should return the stock held by the RepositoryService', () => {
+      // Arrange
+      const expected: Symbol = symbolTestData[1];
+
+      // Act
+      var actual = component.stockWithTicker;
+
+      // Assert
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('getStockAndPrice', () => {
+    it('should call getStockAndPrice on the RepositoryService ', () => {
+      // Arrange
+      const symbolId = 1;
+      const startDate = "2018-01-01";
+      const endDate = "2018-03-01";
+
+      // Act
+      component.getStockAndPrice(symbolId, startDate, endDate);
+
+      // Assert
+      expect(repoServiceSpy.getStockAndPrice).toHaveBeenCalledWith(symbolId, startDate, endDate);
+    });
+  });
+
+  describe('getStockAndPriceWithTicker', () => {
+    it('should call getStockAndPrice on the RepositoryService ', () => {
+      // Arrange
+      const ticker = "IBM";
+      const startDate = "2018-01-01";
+      const endDate = "2018-03-01";
+
+      // Act
+      component.getStockAndPriceWithTicker(ticker, startDate, endDate);
+
+      // Assert
+      expect(repoServiceSpy.getStockAndPriceWithTicker).toHaveBeenCalledWith(ticker, startDate, endDate);
+    });
   });
 });
--- spa/src/app/chapter03/stock-price/stock-price.component.ts
+++ spa/src/app/chapter03/stock-price/stock-price.component.ts
@@ -1,6 +1,6 @@
-import { RepositoryService } from './../../services/repository.service';
 import { Component, OnInit } from '@angular/core';
-
+import { RepositoryService } from 'src/app/services/repository.service';
+import { Symbol } from 'src/app/models/symbol';
 @Component({
   selector: 'app-stock-price',
   templateUrl: './stock-price.component.html',
@@ -9,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
 export class StockPriceComponent implements OnInit {
 
   public title: string = 'Stock Price';
+  private stockId: number = 1;
+  private ticker: string = 'A';
+  private startDate: string = '2018-07-01';
+  private endDate: string = '2018-08-01';
+  private startDateWithTicker: string = '2018-07-01';
+  private endDateWithTicker: string = '2018-08-01';
 
   constructor(private repoService: RepositoryService) { }
 
   ngOnInit() {
+    this.getStockAndPrice(this.stockId, this.startDate, this.endDate);
+    this.getStockAndPriceWithTicker(this.ticker, this.startDateWithTicker, this.endDateWithTicker);
+  }
 
+  public get stock(): Symbol {
+    return this.repoService.stock;
+  }
+
+  public get stockWithTicker(): Symbol {
+    return this.repoService.stockWithTicker;
+  }
+
+  public getStockAndPrice(stockId: number, startDate: string, endDate: string) {
+    this.repoService.getStockAndPrice(stockId, startDate, endDate);
+  }
+
+  public getStockAndPriceWithTicker(ticker: string, startDate: string, endDate: string) {
+    this.repoService.getStockAndPriceWithTicker(ticker, startDate, endDate);
   }
 }
