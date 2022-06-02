23444d4a250c0879d8a82c16c3b685ddb40d4cfd
Aaron <aaron@sevnasoftware.com>
2022-06-02 14:11:03 +0100

    Added content for StockDelete dialog component

--- spa/src/app/chapter03/stock-crud/stock-delete/stock-delete-dialog/stock-delete-dialog.component.spec.ts
+++ spa/src/app/chapter03/stock-crud/stock-delete/stock-delete-dialog/stock-delete-dialog.component.spec.ts
@@ -1,24 +1,65 @@
+import { symbolTestData } from 'src/app/tests/test-data';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IonicModule } from '@ionic/angular';
-
 import { StockDeleteDialogComponent } from './stock-delete-dialog.component';
 
 describe('StockDeleteDialogComponent', () => {
   let component: StockDeleteDialogComponent;
   let fixture: ComponentFixture<StockDeleteDialogComponent>;
+  let matDialogRefSpy: jasmine.SpyObj<MatDialogRef<any, any>>;
 
   beforeEach(waitForAsync(() => {
+    const matDialogRefSpyObj = jasmine.createSpyObj(MatDialogRef, ['close']);
     TestBed.configureTestingModule({
-      declarations: [ StockDeleteDialogComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [StockDeleteDialogComponent],
+      imports: [
+        IonicModule.forRoot(),
+        HttpClientTestingModule,
+        MatDialogModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {provide: MatDialogRef, useValue: matDialogRefSpyObj},
+        { provide: MAT_DIALOG_DATA, useValue: { stock: symbolTestData[0] } }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(StockDeleteDialogComponent);
     component = fixture.componentInstance;
+    
+    matDialogRefSpy = TestBed.inject(MatDialogRef) as jasmine.SpyObj<MatDialogRef<any, any>>;
+
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.stock).toEqual(symbolTestData[0]);
+  });
+
+  describe('close', () => {
+
+    it('should call close on dialogRef passing false', () => {
+      // Arrange
+      // Act
+      component.close();
+
+      // Assert
+      expect(matDialogRefSpy.close).toHaveBeenCalledWith(false);
+    });
+  });
+  describe('deleteStock', () => {
+
+    it('should call close on dialogRef passing true', () => {
+      // Arrange
+      // Act
+      component.deleteStock();
+
+      // Assert
+      expect(matDialogRefSpy.close).toHaveBeenCalledWith(true);
+    });
   });
 });
--- spa/src/app/chapter03/stock-crud/stock-delete/stock-delete-dialog/stock-delete-dialog.component.ts
+++ spa/src/app/chapter03/stock-crud/stock-delete/stock-delete-dialog/stock-delete-dialog.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { Symbol } from 'src/app/models/symbol';
 
 @Component({
   selector: 'app-stock-delete-dialog',
   templateUrl: './stock-delete-dialog.component.html',
   styleUrls: ['./stock-delete-dialog.component.scss'],
 })
-export class StockDeleteDialogComponent implements OnInit {
+export class StockDeleteDialogComponent {
 
-  constructor() { }
+  public stock: Symbol;
 
-  ngOnInit() {}
+  constructor(
+    private dialogRef: MatDialogRef<StockDeleteDialogComponent>, 
+    @Inject(MAT_DIALOG_DATA) data) {
+    this.stock = data.stock;
+  }
 
+  public close(): void {
+    this.dialogRef.close(false);
+  }
+
+  public deleteStock(): void {
+    this.dialogRef.close(true);
+  }
 }
