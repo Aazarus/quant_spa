db2853bc6a66f3e2dac1fc8118b121e3911d04c4
Aaron <aaron@sevnasoftware.com>
2022-06-12 21:05:28 +0100

    Updated app tests

--- spa/src/app/app.component.spec.ts
+++ spa/src/app/app.component.spec.ts
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-label');
-    expect(menuItems.length).toEqual(4);
+    expect(menuItems.length).toEqual(5);
   }));
 
   it('should have urls', waitForAsync(() => {
@@ -35,6 +35,6 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-item');
-    expect(menuItems.length).toEqual(4);
+    expect(menuItems.length).toEqual(5);
   }));
 });
