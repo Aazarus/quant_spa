ca5fcc7e74c42344808a3235d1ff8be1257bc65f
Aaron <aaron@sevnasoftware.com>
2022-06-27 18:38:48 +0100

    Housekeeping: Updated import

--- spa/src/app/services/market-data.service.ts
+++ spa/src/app/services/market-data.service.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { MarketData } from './../models/market-data';
+import { MarketData } from 'src/app/models/market-data';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
