353eccfe1dddfb3c607dbbbacd3779fee3f2d3e1
Aaron <aaron@sevnasoftware.com>
2022-07-16 22:52:55 +0100

    Added content for IEXQuote, inc tests

--- spa/src/app/stock-data/iex/iex-quote/iex-quote.component.spec.ts
+++ spa/src/app/stock-data/iex/iex-quote/iex-quote.component.spec.ts
@@ -1,24 +1,92 @@
+import { IexQuoteTestData } from 'src/app/tests/test-data';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { MarketDataService } from 'src/app/services/market-data.service';
 import { IexQuoteComponent } from './iex-quote.component';
+import { of, throwError } from 'rxjs';
 
 describe('IexQuoteComponent', () => {
   let component: IexQuoteComponent;
   let fixture: ComponentFixture<IexQuoteComponent>;
+  let marketRepoServiceSpy: jasmine.SpyObj<MarketDataService>;
 
   beforeEach(waitForAsync(() => {
+    const marketRepoSpyObj = jasmine.createSpyObj('MarketDataService', ['getIexQuoteWithResult']);
     TestBed.configureTestingModule({
       declarations: [ IexQuoteComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: MarketDataService, useValue: marketRepoSpyObj }
+      ]
+
     }).compileComponents();
 
     fixture = TestBed.createComponent(IexQuoteComponent);
     component = fixture.componentInstance;
+
+    marketRepoServiceSpy = TestBed.inject(MarketDataService) as jasmine.SpyObj<MarketDataService>;
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.data).toBeNull();
+    expect(component.ticker).toBe('');
+  });
+
+  describe('data', () => {
+
+    it('should initially return null', () => {
+
+      // Arrange
+      // Act
+      var actual = component.data;
+
+      // Assert
+      expect(actual).toBeNull();
+    });
+
+    it('should be a getter for _data', () => {
+
+      // Arrange
+      expect(component.data).toBeNull();
+      component['_data'] = IexQuoteTestData;
+      // Act
+      var actual = component.data;
+
+      // Assert
+      expect(actual).toBe(IexQuoteTestData);
+    });
+  });
+
+  describe('getQuote', () => {
+    it('should call repository getIexStock and assign successful result', () => {
+      // Arrange
+      component.ticker = "IBM";
+      spyOn(console, 'log');
+      marketRepoServiceSpy.getIexQuoteWithResult.and.returnValue(of(IexQuoteTestData));
+
+      // ActIexQuoteTestData
+      component.getQuote();
+
+      // Assert
+      expect(marketRepoServiceSpy.getIexQuoteWithResult).toHaveBeenCalled();
+      expect(component.data).toEqual(IexQuoteTestData);
+    });
+
+    // This can be improved to log the error to a third party service for monitoring
+    it('should console.log error', () => {
+      // Arrange
+      component.ticker = "IBM";
+      spyOn(console, 'log');
+      marketRepoServiceSpy.getIexQuoteWithResult.and.returnValue(throwError(""));
+
+      // Act
+      component.getQuote();
+
+      // Assert
+      expect(marketRepoServiceSpy.getIexQuoteWithResult).toHaveBeenCalled();
+      expect(console.log).toHaveBeenCalled();
+    });
   });
 });
--- spa/src/app/stock-data/iex/iex-quote/iex-quote.component.ts
+++ spa/src/app/stock-data/iex/iex-quote/iex-quote.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MarketQuote } from 'src/app/models/market-quote';
+import { MarketDataService } from 'src/app/services/market-data.service';
 
 @Component({
   selector: 'app-iex-quote',
@@ -8,6 +10,19 @@ import { Component } from '@angular/core';
 export class IexQuoteComponent {
 
   public title = "IEX Quote";
+  public ticker: string = "";  
+  private _data: MarketQuote = null;
 
-  constructor() { }
+  constructor(private marketRepo: MarketDataService ) { }
+
+  public get data(): MarketQuote {
+    return this._data;
+  }
+
+  public getQuote(): void {
+    this.marketRepo.getIexQuoteWithResult(this.ticker).subscribe(
+      result => this._data = result,
+      error => console.log(error)
+    );
+  }
 }
