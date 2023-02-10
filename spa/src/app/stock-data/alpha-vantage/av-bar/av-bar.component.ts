067d52b1240f9f3f441a061d2f74f060961c855c
Aaron <aaron@sevnasoftware.com>
2023-02-10 21:29:50 +0000

    Updated av-bar component to display stock bar data

--- spa/src/app/stock-data/alpha-vantage/av-bar/av-bar.component.ts
+++ spa/src/app/stock-data/alpha-vantage/av-bar/av-bar.component.ts
@@ -1,4 +1,6 @@
+import { MarketDataService } from 'src/app/services/market-data.service';
 import { Component, OnInit } from '@angular/core';
+import { MarketData } from 'src/app/models/market-data';
 
 @Component({
   selector: 'app-av-bar',
@@ -8,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class AvBarComponent implements OnInit {
 
   public title: string = "AlphaVantage Bar";
+  public ticker: string = "A";
+  public interval: string = "5";
+  public outputSize: string = "1";
+  public isLoading: boolean = false;
 
-  constructor() { }
+  private _marketData: MarketData[] = [];
 
-  ngOnInit() {}
+  public displayedCols: string[] = ['date', 'open', 'high', 'low', 'close', 'volume'];
 
+  constructor(private marketRepo: MarketDataService) { }
+
+  ngOnInit() {
+    this.getBar();
+  }
+
+  public get stock(): MarketData[] {
+    return this._marketData;
+  }
+
+  public getBar() {
+    this.marketRepo.getAvStockBar(this.ticker, this.interval, this.outputSize).subscribe(
+      result => {
+        this._marketData = result;
+        this.isLoading = false;
+      },
+      error => console.log(error)
+    );
+  }
 }
