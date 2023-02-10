6b75cc63d985584e2d9d20b52cc4da5f5a969248
Aaron <aaron@sevnasoftware.com>
2023-02-10 22:13:18 +0000

    Added new AvQuote interface

---
+++ spa/src/app/models/av-quote.ts
@@ -0,0 +1,12 @@
+export interface AvQuote {
+  ticker: string,
+  timeStamp?: Date,
+  open?: number,
+  high?: number,
+  low?: number,
+  price?: number,
+  volume?: number,
+  prevClose?: number,
+  change?: number,
+  changePercent?: number
+};
