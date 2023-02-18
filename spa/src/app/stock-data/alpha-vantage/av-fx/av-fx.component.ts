c932f59a9351c9dec80b77853966d9e06d0d22b5
Aaron <aaron@sevnasoftware.com>
2023-02-18 14:40:35 +0000

    Added AvFxComponent content

--- spa/src/app/stock-data/alpha-vantage/av-fx/av-fx.component.ts
+++ spa/src/app/stock-data/alpha-vantage/av-fx/av-fx.component.ts
@@ -1,4 +1,6 @@
+import { MarketDataService } from 'src/app/services/market-data.service';
 import { Component, OnInit } from '@angular/core';
+import { FxData } from 'src/app/models/fx-data';
 
 @Component({
   selector: 'app-av-fx',
@@ -8,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 export class AvFxComponent implements OnInit {
 
   public title: string = "AlphaVantage FX EoD";
+  public ticker: string = "";
+  public period: string = "";
+  public startDate: string = "";
+  public startDateLabel: string = "Select a start date";
+  private _marketData: FxData[];
 
-  constructor() { }
+
+  constructor(private repo: MarketDataService) { }
 
   ngOnInit() {}
 
+  public getFxData(): void {
+    this.repo.getAvFx(this.ticker, this.startDate, this.period).subscribe(
+      result => {
+        this._marketData = result;
+      }
+    )
+  }
+
+  public startDateSelected(event: string): void {
+    this.startDate = event
+  }
+
+  public get marketData(): FxData[] {
+    return this._marketData;
+  }
+
+  public get isFormValid(): boolean {
+    return this.ticker === "" ||
+    this.period === "" ||
+    this.startDate === "";
+  }
 }
