b28cb310641e429b06b28a73149bacd4b95ad0bb
Aaron <aaron@sevnasoftware.com>
2023-02-18 21:48:54 +0000

    Added new interface AvSectorPerf

---
+++ spa/src/app/models/av-sector-perf.ts
@@ -0,0 +1,13 @@
+export interface AvSectorPerf {
+  rank?: string;
+  communicationServices?: string;
+  consumerDiscretionary?: string;
+  consumerStaples?: string;
+  energy?: string;
+  financials?: string;
+  healthCare?: string;
+  industrials?: string;
+  informationTechnology?: string;
+  materials?: string;
+  utilities?: string;
+}
