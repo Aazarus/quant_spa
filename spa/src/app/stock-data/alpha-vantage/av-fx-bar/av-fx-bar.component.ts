c98c7bd3cb6623895ff590591349ff13c4416d6c
Aaron <aaron@sevnasoftware.com>
2023-02-18 21:43:27 +0000

    Added content for AvFxBar (API is now a premium subscription with AV)

--- spa/src/app/stock-data/alpha-vantage/av-fx-bar/av-fx-bar.component.ts
+++ spa/src/app/stock-data/alpha-vantage/av-fx-bar/av-fx-bar.component.ts
@@ -1,3 +1,5 @@
+import { MarketDataService } from 'src/app/services/market-data.service';
+import { FxData } from 'src/app/models/fx-data';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class AvFxBarComponent implements OnInit {
 
   public title: string = "AlphaVantage FX Bar";
+  public ticker: string = "";
+  public interval: string = "";
+  public outputSize: string = "";
 
-  constructor() { }
+  private _marketData: FxData[] = [];
+
+  constructor(private repo: MarketDataService) { }
 
   ngOnInit() {}
 
+  public get marketData(): FxData[] {
+    return this._marketData;
+  }
+
+  public get isFormValid(): boolean {
+    return this.ticker === "" ||
+    this.interval === "" ||
+    this.outputSize === "";
+  }
+
+  public getFxBarData(): void {
+    this.repo.getAvFxBar(this.ticker, this.interval, this.outputSize).subscribe(
+      result=>{
+        this._marketData = result;
+      },
+      error => console.log(error)
+    );
+  }
 }
