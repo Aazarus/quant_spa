a8444effe41368751631b02b98e0f5739e398921
Aaron <aaron@sevnasoftware.com>
2023-02-20 22:13:27 +0000

    Added content for QuandlStock

--- spa/src/app/stock-data/quandl/quandl-stock/quandl-stock.component.ts
+++ spa/src/app/stock-data/quandl/quandl-stock/quandl-stock.component.ts
@@ -1,3 +1,6 @@
+import { MarketData } from 'src/app/models/market-data';
+import { QuandlStockData } from './../../../models/quandl-stock-data';
+import { MarketDataService } from 'src/app/services/market-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 export class QuandlStockComponent implements OnInit {
 
   public title: string = "Quandl Stock";
+  public tableTicker: string = "";
+  public ticker: string = "";
+  public startDate: string = "";
+  public startDateLabel: string = "Select a start date";
+  public endDate: string = "";
+  public endDateLabel: string = "Select an end date";
+  public isLoading: boolean = false;
+  private _marketData: QuandlStockData[] = [];
 
-  constructor() { }
+  public displayedCols: string[] = [
+    'date', 'open', 'high', 'low','close', 'volume', 'openAdj', 'highAdj', 'lowAdj', 'closeAdj', 'volumeAdj'
+  ];
+
+  constructor(private repo: MarketDataService) { }
 
   ngOnInit() {}
 
+  public get marketData(): MarketData[] {
+    return this._marketData;
+  }
+
+  public get isFormInvalid(): boolean {
+    return this.ticker === "" ||
+      this.startDate === "" ||
+      this.endDate === "";
+  }
+
+  public startDateSelected(event: string): void {
+    this.startDate = event
+  }
+
+  public endDateSelected(event: string): void {
+    this.endDate = event
+  }
+
+  public async getQuandlStock():  Promise<void> {
+    this.isLoading = true;
+    this.tableTicker = "";
+    this.repo.getQuandlStock(this.ticker, this.startDate, this.endDate).subscribe(
+      result => {
+        this.tableTicker = this.ticker;
+        this.isLoading = false;
+        this._marketData = result;
+      },
+      error => {
+        this.isLoading = false;
+        console.error(error);
+      }
+    )
+  }
 }
