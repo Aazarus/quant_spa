d1dea9b275bed2812db17d8be3aee3177262cb39
Aaron <aaron@sevnasoftware.com>
2023-02-21 20:04:13 +0000

    Added content for new ISDA component (does not include table as Markit have removed the API)

--- spa/src/app/stock-data/isda/isda.component.ts
+++ spa/src/app/stock-data/isda/isda.component.ts
@@ -1,3 +1,5 @@
+import { IsdaRateData } from './../../models/IsdaRateData';
+import { MarketDataService } from 'src/app/services/market-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 export class IsdaComponent implements OnInit {
 
   public title: string = "ISDA";
+  public currency: string = "";
+  public date: string = "";
+  public dateLabel: string = "Select a date";
+  public isLoading: boolean = false;
 
-  constructor() { }
+  private _marketData: IsdaRateData[] = [];
+
+  constructor(private repo: MarketDataService) { }
 
   ngOnInit() {}
 
+  public get marketData(): IsdaRateData[] {
+    return this._marketData;
+  }
+
+  public dateSelected(event: string): void {
+    this.date = event
+  }
+
+  public getIsdaData(): void {
+    this.isLoading = true;
+    this.repo.getIsdaRate(this.currency, this.date).subscribe(
+      result => {
+        this.isLoading = false;
+        this._marketData = result;
+      },
+      error => {
+        this.isLoading = false;
+        console.error(error);
+      }
+    )
+  }
 }
